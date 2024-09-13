55fa034938d6957a8f26a02c8eb746c32164aaa0
Javier Rivera <javierivera29@gmail.com>
2024-09-13 15:14:51 -0600

    arreglado docker compose file para correr en puerto 300

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
-    port: 3000,
-    host: true,
     watch: {
-      usePolling: true,
-    },
+      usePolling: true
+    }
   },
   resolve: {
     alias: {
